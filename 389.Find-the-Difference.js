e2ea06b67d46ae349b8cfafd291b50404ca1d986
Rahul Rajput <rahulrajput21112@gmail.com>
2022-12-03 18:49:51 +0530

    389. Find the Difference

---
+++ 389.Find-the-Difference.js
@@ -0,0 +1,16 @@
+/* Solution of Loot Code Problem : 344. Reverse String */
+
+/**
+ * @param {string} s
+ * @param {string} t
+ * @return {character}
+ */
+ var findTheDifference = function(s, t) {
+    let arr1 = s.split("");
+    let arr2 = t.split("")
+    for (let i = 0; i < arr1.length; i++) {
+        let index = arr2.indexOf(arr1[i]);
+        arr2.splice(index, 1)
+    }
+    return arr2[0]
+};
