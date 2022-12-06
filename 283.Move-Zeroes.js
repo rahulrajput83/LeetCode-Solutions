768f7ae26e9bee31ff52d96d7203cc507816981f
Rahul Rajput <rahulrajput21112@gmail.com>
2022-12-06 15:06:35 +0530

    283. Move Zeroes

---
+++ 283.Move-Zeroes.js
@@ -0,0 +1,16 @@
+/* Solution of Loot Code Problem : 283. Move Zeroes */
+
+/**
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+
+ var moveZeroes = function(nums) {
+    for(let i = nums.length - 1; i >= 0; i--) {
+        if(nums[i] === 0) {
+            nums.splice(i, 1);
+            nums.push(0)
+        }
+    };
+     return nums;
+ };
