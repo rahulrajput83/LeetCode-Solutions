693c2030c3676314d747764a2bd55e1511837e01
Rahul Rajput <rahulrajput21112@gmail.com>
2022-12-10 19:22:00 +0530

    1822. Sign of the Product of an Array

---
+++ 1822.Sign-of-the-Product-of-an-Array.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var arraySign = function(nums) {
+    let product = 1;
+    for(let i = 0; i < nums.length; i++) {
+        product = product * nums[i];
+        
+    }
+    if(product >= 1) { 
+        return 1
+    }
+    else if(product < 0) {
+        return -1
+    }
+    else {
+        return 0
+    }
+};
